ee3231945a984bdc80ab9e89d76a8da42cb42c72
Matheus Mazoni <mmazoni.andrade@gmail.com>
2020-08-03 00:01:22 +0000

    required job attribute tests

--- tests/Feature/JobTest.php
+++ tests/Feature/JobTest.php
@@ -9,7 +9,7 @@ use Tests\TestCase;
 class JobTest extends TestCase
 {
     use WithFaker, RefreshDatabase;
-    
+
     public function testCreateJob()
     {
         $this->withoutExceptionHandling();
@@ -25,9 +25,37 @@ class JobTest extends TestCase
         ];
 
         $this->post('/job', $attributes);
-
         $this->assertDatabaseHas('jobs', $attributes);
-
         $this->get('/job')->assertSee($attributes['company_name']);
+    }
+
+    public function testRequiredName()
+    {
+        $response = $this->post('/job', [
+            'company_name' => $this->faker->name,
+            'description' => '',
+            'role' => $this->faker->name,
+            'level' => $this->faker->sentence,
+            'requirements' => $this->faker->paragraph,
+            'location' => $this->faker->sentence,
+            'benefits' => $this->faker->sentence,
+        ]);
+
+        $response->assertSessionHasErrors('description');
+    }
+
+    public function testRequiredDescription()
+    {
+        $response = $this->post('/job', [
+            'company_name' => '',
+            'description' => $this->faker->paragraph,
+            'role' => $this->faker->name,
+            'level' => $this->faker->sentence,
+            'requirements' => $this->faker->paragraph,
+            'location' => $this->faker->sentence,
+            'benefits' => $this->faker->sentence,
+        ]);
+
+        $response->assertSessionHasErrors('company_name');
     }
 }
